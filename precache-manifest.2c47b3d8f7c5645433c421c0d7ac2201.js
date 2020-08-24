4bc0cff6c64588462c31df21fc11b6be32a48b7f
Yusuf <yusuf@Yusufs-MacBook-Pro.local>
2020-08-23 21:25:35 -0400

    Updates

---
+++ precache-manifest.2c47b3d8f7c5645433c421c0d7ac2201.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916e505b0ea1ba00a2e5116e89952864",
+    "url": "/covidTracker/index.html"
+  },
+  {
+    "revision": "5a46e49518de7fb5a066",
+    "url": "/covidTracker/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bf7e9e9bec1631cd5cf0",
+    "url": "/covidTracker/static/css/main.defdd5ca.chunk.css"
+  },
+  {
+    "revision": "5a46e49518de7fb5a066",
+    "url": "/covidTracker/static/js/2.12fd54f1.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covidTracker/static/js/2.12fd54f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf7e9e9bec1631cd5cf0",
+    "url": "/covidTracker/static/js/main.0a2e3200.chunk.js"
+  },
+  {
+    "revision": "d761046bc21590e26ab7",
+    "url": "/covidTracker/static/js/runtime-main.199b7a58.js"
+  }
+]);
